30e4ce8cf0779ca222318a357435478d158c10e0
Laure Cougnaud <laure.cougnaud@openanalytics.eu>
2024-03-09 16:20:02 +0000

    version 3.3.2

--- tests/testthat/test_subjectProfileSummaryPlot-general.R
+++ tests/testthat/test_subjectProfileSummaryPlot-general.R
@@ -1,6 +1,7 @@
 context("Create a subject profile summary plot")
 
 library(ggplot2)
+library(plyr)
 
 test_that("An error is generated if the default variable for the plot is not available in the data", {
 			
@@ -219,7 +220,7 @@ test_that("A table with specific height is correctly included in a plot", {
 	)
 	
 	ggData <- ggplot_build(gg)$data
-	ggData <- do.call(rbind.fill, ggData)
+	ggData <- do.call(plyr::rbind.fill, ggData)
 	
 	# 2 panels are created
 	expect_equal(nrow(ggData), 2)
